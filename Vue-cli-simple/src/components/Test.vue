f17b2b3651ea711f38b8c685badf1808a4f0101a
fangwk <fwk1010@github.com>
2018-01-11 22:25:39 +0800

    rm day01

---
+++ Vue-cli-simple/src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'new vue file'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
--- helloWorld/app.js
+++
@@ -1,6 +0,0 @@
-var ser = require('http');
-ser.createServer(function(req, rep) {
-    rep.writerheader(200, { "content-type": "text/plain" });
-    rep.writer("hhh");
-    rep.writerend();
-}).listener(3000);
